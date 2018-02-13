8e6ac4e21d1e59aaf350040df8cadb1c814ecd6f
hunglq <hunglq@rikkeisoft.com>
2018-02-13 16:47:11 +0700

    update

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
  * @license MIT
  */
 
-const raf = require('raf')
-const objectAssign = require('object-assign')
-const kindOf = require('kind-of')
+var raf = require('raf')
+var objectAssign = require('object-assign')
+var kindOf = require('kind-of')
 
 /**
  * Create a handler for synchronizing items
@@ -17,70 +17,70 @@ const kindOf = require('kind-of')
  * @param {Function} options.setter required, is function, set value into items
  * @param {integer} options.duration optional, milliseconds, duration of synchronizing operation, default value is 2000
  */
-class ParrotsHandler {
-  constructor (options) {
-    const defaultOptions = {
-      duration: 2000
-    }
-
-    // Affected items (receive copied value from source), passed as first arguments of setter
-    this.items = []
-
-    // Source to copy from, passed as the only argument of options.getter
-    this.source = null
-
-    // Timer of synchronizing operations
-    this.timer = null
-
-    // options
-    this.options = objectAssign({}, defaultOptions, options)
-
-    if (kindOf(this.options.getter) !== 'function') {
-      throw new Error('[ParrotsHandler] options.getter should be function')
-    }
-
-    if (kindOf(this.options.setter) !== 'function') {
-      throw new Error('[ParrotsHandler] options.setter should be function')
-    }
-
-    // events
-    this.startLoopEvent = this.startLoop.bind(this)
-    this.freeLoopEvent = this.freeLoop.bind(this)
-    this.loopEvent = this.loop.bind(this)
+var ParrotsHandler = function (options) {
+  var defaultOptions = {
+    duration: 2000
   }
 
+  // Affected items (receive copied value from source), passed as first arguments of setter
+  this.items = []
+
+  // Source to copy from, passed as the only argument of options.getter
+  this.source = null
+
+  // Timer of synchronizing operations
+  this.timer = null
+
+  // options
+  this.options = objectAssign({}, defaultOptions, options)
+
+  if (kindOf(this.options.getter) !== 'function') {
+    throw new Error('[ParrotsHandler] options.getter should be function')
+  }
+
+  if (kindOf(this.options.setter) !== 'function') {
+    throw new Error('[ParrotsHandler] options.setter should be function')
+  }
+
+  // events
+  this.startLoopEvent = this.startLoop.bind(this)
+  this.freeLoopEvent = this.freeLoop.bind(this)
+  this.loopEvent = this.loop.bind(this)
+}
+
+ParrotsHandler.prototype = {
   /**
    * Add an affected item (receive synchronized value from source)
    * @param {object} item item that receives synchronized value from source
    * @return {this}
    */
-  to (item) {
+  to: function (item) {
     this.items.push(item)
     return this
-  }
+  },
 
   /**
    * Get a trigger to start synchronizing value from source into items
    * @param {object} source source of synchronized value passed into items
    * @return {Function}
    */
-  from (source) {
+  from: function (source) {
     return () => {
       this.source = source
       return this.startLoopEvent()
     }
-  }
+  },
 
   // Loop used for synchronizing items
-  loop () {
+  loop: function () {
     this.sync()
 
     // Recall loop in a performant style
     raf(this.loopEvent)
-  }
+  },
 
   // Sync items
-  sync () {
+  sync: function () {
     // Get copied value from source
     const copiedValue = this.options.getter(this.source)
     // Copy value to every items in stack
@@ -88,20 +88,20 @@ class ParrotsHandler {
       // Use setter to copy into affected item
       this.options.setter(this.items[idx], copiedValue)
     }
-  }
+  },
 
   // Start loop
-  startLoop () {
+  startLoop: function () {
     if (this.timer === null) {
       // create new timer
       this.timer = raf(this.loopEvent)
       // release timer when time is up
       setTimeout(this.freeLoopEvent, this.loopDuration)
     }
-  }
+  },
 
   // End loop
-  freeLoop () {
+  freeLoop: function () {
     if (this.timer !== null) {
       // Release timer
       raf.cancel(this.timer)
@@ -125,7 +125,9 @@ class ParrotsHandler {
  *  setter: (el, value) => $(el).scrollLeft(value)
  * })
  */
-const parrots = (options) => new ParrotsHandler(options)
+var parrots = function (options) {
+  return new ParrotsHandler(options)
+}
 
 exports = module.exports = parrots
 exports.ParrotsHandler = ParrotsHandler
