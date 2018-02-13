3cee635afa384d5027182705f7f4974e6469509d
hunglq <hunglq@rikkeisoft.com>
2018-02-13 11:20:56 +0700

    complete build and test process

--- README.md
+++ README.md
@@ -18,14 +18,14 @@ var syncScrollLeft = parrots({
   getter: (el) => $(el).scrollLeft(),
   // set scroll left into items
   setter: (el, value) => $(el).scrollLeft(value)
+  // duration, optional, default is 2 seconds (2000 milliseconds)
+  duration: 2000
 })
 
 // Add affected elements
 syncScrollLeft.to(el)
 syncScrollLeft.to(el2)
+
 // Trigger scroll left copying from el3 when it's scrolled
 el3.on('scroll', syncScrollLeft.from(el3))
 ```
-
-## API
-Please view [API.md](https://github.com/hungluu2106/parrots/blob/master/API.md)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const util = require('util')
 
 const package = require('./package.json')
 const packageName = package.name
@@ -7,30 +8,75 @@ const packageFileMin = packageName + '.min.js'
 
 module.exports = {
   entry: {
-    packageFile : './entry.js',
-    packageFileMin : './entry.js'
+    [packageFile] : './index.js',
+    [packageFileMin] : './index.js'
   },
   output: {
     path: __dirname + '/dist',
     filename: '[name]'
   },
-  devtool: 'source-map',
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: __dirname,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'stage-0']
-        }
+        loader: 'babel-loader' // use .babelrc
       }
     ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/,
-      minimize: true
-    })
+      compress: {
+        warnings: false,
+        properties: true,
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        comparisons: true,
+        evaluate: true,
+        booleans: true,
+        unused: true,
+        loops: true,
+        hoist_funs: true,
+        cascade: true,
+        if_return: true,
+        join_vars: true,
+        //drop_console: true,
+        drop_debugger: true,
+        unsafe: true,
+        hoist_vars: true,
+        negate_iife: true,
+        //side_effects: true
+      },
+      sourceMap: true,
+      mangle: {
+        toplevel: true,
+        sort: true,
+        eval: true,
+        properties: true
+      },
+      output: {
+        space_colon: false,
+        comments: function(node, comment) {
+          var text = comment.value
+          var type = comment.type
+          if (type == "comment2") {
+            // multiline comment
+            return /@copyright/i.test(text)
+          }
+        }
+      }
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      include: /\.min\.js$/,
+      filename: "[file].map"
+    }),
+    new webpack.BannerPlugin(
+      {
+        banner: util.format('%s v%s | %s', package.name, package.version, package.author),
+        entryOnly: true
+      }
+    )
   ]
 }
